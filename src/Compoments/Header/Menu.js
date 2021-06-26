95ba313fa913630dc3c25473fd235141f0b8efe0
Ahmet Kutuk <ahmetcankutk@gmail.com>
2021-06-26 03:01:12 +0300

    Menu right margin edit

--- src/Compoments/Header/Menu.js
+++ src/Compoments/Header/Menu.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Menu() {
   return (
-    <div className="flex mt-6 mr-4 md:mt-9 self-end lg:mr-52 ">
+    <div className="flex mt-6 mr-4 md:mt-9 self-end lg:mr-48 ">
       <div className="menu justify-self-end" style={{ color: '#1F3F68' }}>
         <a href="sf" className="mr-7">
           About
