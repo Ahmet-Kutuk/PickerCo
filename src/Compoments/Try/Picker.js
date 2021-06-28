809505252555c0f5017e92c30312c9a552268ce3
Ahmet Kutuk <ahmetcankutk@gmail.com>
2021-06-29 01:01:09 +0300

    Picker compoment responsive fix

--- src/Compoments/Try/Picker.js
+++ src/Compoments/Try/Picker.js
@@ -4,13 +4,13 @@ function Picker() {
   return (
     <>
       <div className="flex try-section justify-center lg:p-3 mt-4 lg:mt-10 mb-10">
-        <form className="picker-form">
+        <form className="picker-form flex flex-col justify-center items-center md:flex-row">
           <input
             type="text"
             placeholder="Example Youtube ID = 5-MT5zeY6CU"
             className="rounded-lg px-2 picker-input py-5 outline-none placeholder-blue-500 "
           />
-          <button className="bg-blue-500 w-48 py-5 mt-5 lg:mt-0  rounded-lg text-white ml-10">
+          <button className="bg-blue-500 w-48 py-5 mt-5 lg:mt-0 rounded-lg text-white lg:ml-10">
             Pick
           </button>
         </form>
