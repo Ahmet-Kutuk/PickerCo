506f42e3e2a8b7b729c55c84f81ed461e2233cd7
Ahmet Kutuk <ahmetcankutk@gmail.com>
2021-06-29 21:42:36 +0300

    Load spinner add modal

--- src/Compoments/Try/Winner.js
+++ src/Compoments/Try/Winner.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
-
+import DotLoader from 'react-spinners/DotLoader';
 Modal.setAppElement('#root');
 function Winner(props) {
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <>
       <Modal
@@ -34,20 +35,31 @@ function Winner(props) {
           },
         }}
       >
-        <button className="self-end" onClick={() => props.rePick()}>
-          <i className="fas fa-window-close text-3xl"></i>
-        </button>
-
-        <div className="rounded-full profile"></div>
-        <div className="nickname">
-          <h2 className="text-xl ">sdf</h2>
-        </div>
-        <button
-          className="p-4 bg-blue-500 text-white rounded-lg w-48 mt-52"
-          onClick={() => props.rePick()}
-        >
-          Re Pick
-        </button>
+        {isLoading ? (
+          <>
+            <button className="self-end" onClick={() => props.rePick()}>
+              <i className="fas fa-window-close text-3xl"></i>
+            </button>
+            <h1 className="mb-16">Winner Loading</h1>
+            <DotLoader color={'blue'} loading={isLoading} size={100} />
+          </>
+        ) : (
+          <>
+            <button className="self-end" onClick={() => props.rePick()}>
+              <i className="fas fa-window-close text-3xl"></i>
+            </button>
+            <div className="rounded-full profile"></div>
+            <div className="nickname">
+              <h2 className="text-xl ">sdf</h2>
+            </div>
+            <button
+              className="p-4 bg-blue-500 text-white rounded-lg w-48 mt-52"
+              onClick={() => props.rePick()}
+            >
+              Re Pick
+            </button>
+          </>
+        )}
       </Modal>
     </>
   );
